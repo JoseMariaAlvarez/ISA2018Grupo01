9431c83880d954b7937b76abbf9f135aeaf6eec4
mmp110000000 <marcomunozperez@uma.es>
2018-03-07 09:36:35 +0100

    Installation of django

---
+++ servidor/backend/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/marcomunozperez/Projects/IS/ISA2018Grupo01/servidor/backend/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
